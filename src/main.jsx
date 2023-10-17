d611c244ffdc504f0997017ef5803531996cd4f0
JAL117 <106935663+JAL117@users.noreply.github.com>
2023-10-17 09:43:58 -0600

    Enrutamiento del proyecto

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,82 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {  createBrowserRouter, RouterProvider,} from "react-router-dom";
+import Layout from './pages/Layout';
+import LayoutInicial from './pages/LayoutInicial'
+import Panel from './pages/Panel'
+import User from './pages/User'
+import AgregarUsuario from './pages/AgregarUsuario'
+import Empleados from './pages/Empleados'
+import Cocina from './pages/Cocina'
+import Ordenes from './pages/Ordenes'
+import Login from './pages/Login'
+import MenuDital from './pages/MenuDital';
+
+const router = createBrowserRouter([
+{
+  path: '/inicio',
+  element: <Layout/>,
+  children: [
+    {
+      index: true,
+      path:'usuario',
+      element:<User/>
+    },
+    {
+      path:"panel",
+      element:<Panel/>
+
+    },
+    {
+      path:"agregarusuarios",
+      element:<AgregarUsuario/>
+
+    },
+    {
+      path:"empleados",
+      element:<Empleados/>
+
+    },
+    {
+      path:"cocina",
+      element:<Cocina/>
+
+    },
+    {
+      path:"ordenes",
+      element:<Ordenes/>
+
+    },
+    {
+      path:"menudigital",
+      element:<MenuDital/>
+
+    }
+  
+  
+  
+  
+  
+  
+
+  ]
+},  
+{
+  path:"/",
+  element:<LayoutInicial/>,
+  children:[{
+  index: true,
+  element:<Login/>
+
+  }]
+  
+
+}
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router={router}/>  
   </React.StrictMode>,
 )
