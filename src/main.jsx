ceccc87801425dd815d29e7d890d3f99e1291693
saul201287 <jssaul99@gmail.com>
2023-10-20 19:57:57 -0600

    configuracion de la validacion de los usuarios en el login

--- src/main.jsx
+++ src/main.jsx
@@ -1,81 +1,78 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {  createBrowserRouter, RouterProvider,} from "react-router-dom";
-import Layout from './pages/Layout';
-import LayoutInicial from './pages/LayoutInicial'
-import Panel from './pages/Panel'
-import User from './pages/User'
-import AgregarUsuario from './pages/AgregarUsuario'
-import Empleados from './pages/Empleados'
-import Cocina from './pages/Cocina'
-import Ordenes from './pages/Ordenes'
-import Login from './pages/Login'
-import MenuDital from './pages/MenuDital';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./pages/Layout";
+import LayoutInicial from "./pages/LayoutInicial";
+import Panel from "./pages/Panel";
+import User from "./pages/User";
+import AgregarUsuario from "./pages/AgregarUsuario";
+import Empleados from "./pages/Empleados";
+import Cocina from "./pages/Cocina";
+import Ordenes from "./pages/Ordenes";
+import Login from "./pages/Login";
+import MenuDital from "./pages/MenuDital";
+import Envios from "./pages/Envios";
 
 const router = createBrowserRouter([
-{
-  path: '/inicio',
-  element: <Layout/>,
-  children: [
-    {
-      index: true,
-      path:'usuario',
-      element:<User/>
-    },
-    {
-      path:"panel",
-      element:<Panel/>
+  {
+    path: "/inicio",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        path: "usuario",
+        element: <User />,
+      },
+      {
+        path: "panel",
+        element: <Panel />,
+      },
+      {
+        path: "agregarusuarios",
+        element: <AgregarUsuario />,
+      },
+      {
+        path: "empleados",
+        element: <Empleados />,
+      },
+      {
+        path: "cocina",
+        element: <Cocina />,
+      },
+      {
+        path: "ordenes",
+        element: <Ordenes />,
+      },
+      {
+        path: "menudigital",
+        element: <MenuDital />,
+      },
+    ],
+  },
+  {
+    path: "/",
+    element: <LayoutInicial />,
+    children: [
+      {
+        index: true,
+        element: <Login />,
+      },
+    ],
+  },
+  {
+    path: "/MenuEnvios",
+    element: <Envios />,
+    children: [
+      {
+        index: true,
+        element: <Login />,
+      },
+    ],
+  },
+]);
 
-    },
-    {
-      path:"agregarusuarios",
-      element:<AgregarUsuario/>
-
-    },
-    {
-      path:"empleados",
-      element:<Empleados/>
-
-    },
-    {
-      path:"cocina",
-      element:<Cocina/>
-
-    },
-    {
-      path:"ordenes",
-      element:<Ordenes/>
-
-    },
-    {
-      path:"menudigital",
-      element:<MenuDital/>
-
-    }
-  
-  
-  
-  
-  
-  
-
-  ]
-},  
-{
-  path:"/",
-  element:<LayoutInicial/>,
-  children:[{
-  index: true,
-  element:<Login/>
-
-  }]
-  
-
-}
-])
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-   <RouterProvider router={router}/>  
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/pages/Envios.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CardsMenu from '../components/MenuDigital/CardsMenu'
+import ComandaMenuDigital from '../components/MenuDigital/ComandaMenuDigital'
+
+function Envios(props) {
+    return (
+        <div>
+           <CardsMenu/>
+        </div>
+    );
+}
+
+export default Envios;
