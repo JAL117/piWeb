d0e2906b45cd6dfb961d75d83e55fd2d7d7d4c80
JAL117 <jagvms117@gmail.com>
2023-11-06 22:03:57 -0600

    Actualizaciones de vistas v1.5

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,16 +1,15 @@
 import { Outlet, useLocation } from "react-router-dom";
 import SideMenu from "../components/Principal/SideMenu";
-import Footer from "../components/Principal/Footer";
 import '../App.css'
 
 function Layout() {
   return (
     <div>
       <SideMenu/>
-      <div style={{marginTop:"8%",marginLeft:"5%" , marginBottom:"15%"}}>
+      <div style={{marginTop:"6%",marginLeft:"5%"}}>
         <Outlet/>
       </div>
-      <Footer/>
+   
     </div>
   );
 }
