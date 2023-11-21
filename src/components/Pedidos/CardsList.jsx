2e9dc30f3d805c06d77d8872dc659021dfda800b
JAL117 <jagvms117@gmail.com>
2023-11-20 20:53:15 -0600

    Cambios vistas

--- src/components/Pedidos/CardsList.jsx
+++ src/components/Pedidos/CardsList.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { BsFillCartCheckFill } from "react-icons/bs";
 import Animaciones from "../utils/Animaciones";
+import Swal from "sweetalert2";
 
 function CardList({ alimentos, categoriaActual }) {
   const alimentosFiltrados =
@@ -12,26 +13,66 @@ function CardList({ alimentos, categoriaActual }) {
       ? alimentos.filter((alimento) => alimento.categoria === categoriaActual)
       : alimentos;
 
+  const [cantidad, setCantidad] = useState(0);
+
+  const handleEnviar = (alimento) => {
+    Swal.fire({
+      title: "Ingrese la cantidad",
+      input: "number",
+      inputAttributes: {
+        min: 1,
+        max: 50,
+      },
+      showCancelButton: true,
+      confirmButtonText: "Enviar",
+      cancelButtonText: "Cancelar",
+      reverseButtons: true,
+      allowOutsideClick: false,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const cantidad = parseInt(result.value);
+        if (isNaN(cantidad) || cantidad <= 0 || cantidad > 50) {
+          Swal.fire(
+            "Cantidad inválida",
+            "Ingrese una cantidad válida.",
+            "error"
+          );
+        } else {
+          setCantidad(cantidad);
+          Swal.fire(
+            "Cantidad enviada",
+            `Cantidad: ${cantidad}`,
+            "success"
+          );
+          // Aquí puedes realizar el envio del dato michi
+        }
+      }
+    });
+  };
+
   return (
     <Row className="g-2 p-2 mt-2">
       {alimentosFiltrados.length > 0 ? (
         alimentosFiltrados.map((alimento) => (
           <Col key={alimento.id} xs={12} sm={6} md={4} lg={4}>
             <Animaciones>
-              <Card style={{ backgroundColor: "rgba(209, 35, 35, 0.1)",  boxShadow: "0px 0px 15px 3px rgba(0, 0, 0, 0.1)" }}>
-                {alimento.imagen && (
-                  <Card.Img variant="top" src={alimento.imagen} />
-                )}
+              <Card
+                style={{
+                  backgroundColor: "rgba(209, 35, 35, 0.1)",
+                  boxShadow: "0px 0px 15px 3px rgba(0, 0, 0, 0.1)",
+                }}
+              >
                 <Card.Body className="d-flex justify-content-around">
                   <div>
-                    <Card.Title>{alimento.nombre}</Card.Title>
+                    <Card.Title>{alimento.nombre}{alimento.precio}</Card.Title>
                   </div>
                   <Button
                     variant="success"
                     className="ms-2 mt-2"
                     style={{ height: "50%" }}
+                    onClick={() => handleEnviar(alimento)}
                   >
-                    <BsFillCartCheckFill size={20} />
+                    <BsFillCartCheckFill size={20} /> Enviar
                   </Button>
                 </Card.Body>
               </Card>
