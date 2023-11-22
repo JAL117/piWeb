5bb6b9a865ec43fa5ba011db2adf119ace29695a
JAL117 <jagvms117@gmail.com>
2023-11-22 16:21:54 -0600

    Cambios en panel

---
+++ src/components/Panel/GraficaPastel.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Chart, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+
+Chart.register(ArcElement, Tooltip, Legend);
+
+const options = {
+    responsive: true,
+    fill: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "Productos mas vendidos",
+      }
+    }
+  };
+
+const data = {
+  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
+function GraficaPastel() {
+  return <Pie data={data} options={options} style={{height:'50%' , width:'50%'}}/>;
+}
+
+export default GraficaPastel;
--- src/pages/Panel.jsx
+++ src/pages/Panel.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import PanelGrafica from '../components/Panel/PanelGrafica'
 import Ventas from '../components/Panel/Ventas'
+import { Row } from 'react-bootstrap'
 
 function Panel() {
   return (
-    <div style={{marginLeft:'15%'}}>
-      <div style={{display: 'flex'}}>
-        <Ventas></Ventas>
+    <Row className='ms-5'>
+      <div className='mt-2 ms-5'>
+        <Ventas/>
       </div>
-      <div>
+      <div className='p-2 mt-5'>
             <PanelGrafica/>
       </div>
-    </div>
+    </Row>
   )
 }
 
