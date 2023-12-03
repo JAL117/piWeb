d6775cb6cb84f3535f4e4c4c2245f57695751144
JAL117 <jagvms117@gmail.com>
2023-12-03 17:32:06 -0600

    Notificación de pedido realizado por cocina en el apartado de pedidos de sucursal

--- src/components/Sucursal/Sucursal.jsx
+++ src/components/Sucursal/Sucursal.jsx
@@ -1,34 +1,37 @@
 import React, { useState, useEffect } from "react";
 import OrderCard from "./OrdenesSucursal";
 import axios from "axios";
-import io from "socket.io-client"
+import io from "socket.io-client";
 import Swal from "sweetalert2";
 
 const socket = io("http://localhost:3006");
+
 function OrdersList() {
   const [ordenes, setOrdenes] = useState([]);
 
   const pedidos = async () => {
-    await axios
-      .get("http://localhost:3006/pedidos/local")
-      .then((data) => {
-        setOrdenes(data.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    try {
+      const response = await axios.get("http://localhost:3006/pedidos/local");
+      setOrdenes(response.data);
+    } catch (error) {
+      console.log(error);
+    }
   };
+
   useEffect(() => {
     pedidos();
-    socket.on("pedidoEnviado", () =>{
+    socket.on("pedidoEnviado", () => {
       console.log("pedido Enviado");
-      Swal.fire({
-        title: "Pedido completado en cocina",
-        text: "Completado pedido para mesa #",
-        icon: "succes",
-      })
-    })
-  }, []);
+      const lastOrder = ordenes[ordenes.length - 1];
+      if (lastOrder) {
+        Swal.fire({
+          title: "Pedido realizado en cocina",
+          text: `Para mesa #${lastOrder.mesa}`,
+          icon: "success",
+        });
+      }
+    });
+  }, [ordenes]); 
 
   return (
     <>
@@ -36,7 +39,13 @@ function OrdersList() {
       <div className="row  row-cols-lg-3">
         {ordenes.map((order, index) => (
           <div className="col mb-1" key={index}>
-            <OrderCard order={order} index={index} pedidos={pedidos} />
+            <OrderCard
+              order={order}
+              index={index}
+              pedidos={pedidos}
+              orderId={order._id}
+              mesa={order.mesa}
+            />
           </div>
         ))}
       </div>
